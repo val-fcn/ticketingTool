9dc07c3316f2ff32b6568c59da23726dfec05c4f
val-fcn <valentin.faucon@gmail.com>
2025-01-10 11:48:09 +0100

    auto redirect to login

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <UContainer class="min-h-screen flex items-center justify-center">
+        <h1 class="text-2xl font-bold">Chargement...</h1>
+    </UContainer>
+  </template>
+  
+  <script setup>
+  import { onMounted } from 'vue'
+  import { navigateTo } from '#app'
+
+  onMounted(() => {
+    navigateTo('/login')
+  })
+  </script>
